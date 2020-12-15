0d5c464e125d792fc6b9830ad5540d6621a34595
younes khoussi <younes.khoussi@edu.uiz.ac.ma>
2020-12-16 00:49:34 +0100

    Preparation du projet

---
+++ src/main/java/me/yns/SpringBatchBank/SpringBatchBankApplication.java
@@ -0,0 +1,13 @@
+package me.yns.SpringBatchBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yns/SpringBatchBank/SpringBatchBankApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yns.SpringBatchBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
